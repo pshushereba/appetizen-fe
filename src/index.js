081f93417c7774e4023b2a109156d903614b7f12
Patrick Shushereba <pshushereba@gmail.com>
2020-11-03 19:32:35 -0800

    Working on webpack config.

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+// const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
