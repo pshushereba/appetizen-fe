80d236f9f1f59703362ab7bc00e13074ea0ed012
Patrick Shushereba <pshushereba@gmail.com>
2020-11-11 20:21:46 -0800

    Rendered video in LiveStream component.

--- src/components/LiveStream.js
+++ src/components/LiveStream.js
@@ -4,6 +4,17 @@ import axios from "axios";
 import io from "socket.io-client";
 import { PlayArrow } from "@material-ui/icons";
 
+let videoGrid;
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
+console.log(videoGrid);
+
 const LiveStream = () => {
   const [chat, setChat] = useState([]);
   const [videoStream, setVideoStream] = useState(null);
@@ -15,25 +26,29 @@ const LiveStream = () => {
   });
 
   useEffect(() => {
+    videoGrid = document.getElementById("video-grid");
+    const myVideo = document.createElement("video");
+    myVideo.muted = true;
     navigator.mediaDevices
       .getUserMedia({
         video: true,
         audio: true,
       })
       .then((stream) => {
-        setVideoStream(stream);
+        addVideoStream(myVideo, stream);
       });
   }, []);
 
   videoSocket.on("connection", () => {
     videoSocket.emit("the client is connected");
   });
-  console.log(videoStream);
+  // console.log(videoStream);
 
   return (
-    <div>
-      <h1>LiveStream</h1>
-      <video muted={true} src={videoStream} width="250" autoplay />
+    <div id="video-grid">
+      {/* <h1>LiveStream</h1> */}
+      <div></div>
+      {/* <video muted={true} src={videoStream} width="250" autoplay /> */}
     </div>
   );
 };
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,11 +1,195 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Divider from "@material-ui/core/Divider";
+import { makeStyles } from "@material-ui/core/styles";
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
+import { useParams } from "react-router-dom";
+import { connect } from "react-redux";
+import { updateAccount } from "../actions/index";
 
-const Settings = () => {
+const lightColor = "rgba(255, 255, 255, 0.7)";
+
+const useStyles = makeStyles((theme) => ({
+  secondaryBar: {
+    zIndex: 0,
+  },
+  menuButton: {
+    marginLeft: -theme.spacing(1),
+  },
+  iconButtonAvatar: {
+    padding: 4,
+  },
+  link: {
+    textDecoration: "none",
+    color: lightColor,
+    "&:hover": {
+      color: theme.palette.common.white,
+    },
+  },
+  button: {
+    borderColor: lightColor,
+  },
+  paper: {
+    padding: 0,
+  },
+  divider: {
+    //
+  },
+}));
+
+const Settings = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [updatedAccount, setUpdatedAccount] = useState({
+    id: props.account.id,
+    first_name: "",
+    last_name: "",
+    address: "",
+    address2: "",
+    city: "",
+    state: "",
+    zip: "",
+    case_number: "",
+    case_type: "",
+    party_name: "",
+  });
+
+  const { classes } = props;
+  // console.log(props);
+
+  useEffect(() => {
+    setUpdatedAccount(props.account);
+  }, []);
+
+  // console.log(props.account);
+
+  const handleChange = (event) => {
+    setUpdatedAccount({
+      ...updatedAccount,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    updateAccount(props.account.id, updatedAccount);
+  };
+
+  console.log(updatedAccount);
+
   return (
-    <div>
-      <h1>Settings</h1>
-    </div>
+    <Paper className={classes.paper}>
+      <AppBar
+        component="div"
+        className={classes.secondaryBar}
+        color="primary"
+        position="static"
+        elevation={0}
+      ></AppBar>
+      <AppBar
+        component="div"
+        className={classes.secondaryBar}
+        color="primary"
+        position="fixed"
+        elevation={0}
+      >
+        {/* Need to fix the position of this. */}
+        <Tabs value={0} textColor="inherit">
+          <Tab textColor="inherit" label="Case Settings" />
+          <Tab textColor="inherit" label="Account Settings" />
+        </Tabs>
+      </AppBar>
+      <form onSubmit={handleSubmit}>
+        <fieldset disabled={isEditing}>
+          <Grid container direction="column">
+            <Grid item sm>
+              <Typography>Personal Information</Typography>
+              <Divider />
+              <TextField
+                name="first_name"
+                label="First Name"
+                value={updatedAccount.first_name}
+                onChange={handleChange}
+              />
+              <TextField
+                name="last_name"
+                label="Last Name"
+                value={updatedAccount.last_name}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item sm>
+              <TextField
+                name="address"
+                label="Address"
+                value={updatedAccount.address}
+                onChange={handleChange}
+              />
+              <TextField
+                name="address2"
+                label="Apt/Unit/PO Box"
+                value={updatedAccount.address2}
+                onChange={handleChange}
+              />
+              <TextField
+                name="city"
+                label="City"
+                value={updatedAccount.city}
+                onChange={handleChange}
+              />
+              <TextField
+                name="state"
+                label="State"
+                value={updatedAccount.state}
+                onChange={handleChange}
+              />
+              <TextField
+                name="zip"
+                label="Zip Code"
+                value={updatedAccount.zip}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item sm>
+              <Typography>Case Information</Typography>
+              <Divider />
+              <TextField
+                name="case_number"
+                label="Case Number"
+                value={updatedAccount.case_number}
+                onChange={handleChange}
+              />
+              <TextField
+                name="case_type"
+                label="Case Type"
+                value={updatedAccount.case_type}
+                onChange={handleChange}
+              />
+              <TextField
+                name="party_name"
+                label="Party Name"
+                value={updatedAccount.party_name}
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
+          <Button onClick={() => setIsEditing(!isEditing)}>Edit</Button>
+          <Button type="submit">Update Account</Button>
+        </fieldset>
+      </form>
+    </Paper>
   );
 };
 
-export default Settings;
+const mapStateToProps = (state) => {
+  return {
+    account: state.accountReducer.account,
+  };
+};
+
+export default connect(mapStateToProps, { updateAccount })(Settings);
