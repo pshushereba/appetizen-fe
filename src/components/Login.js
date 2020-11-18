034e9363488fd519af3ea456854d8e136c0e818e
Patrick Shushereba <pshushereba@gmail.com>
2020-11-18 09:11:00 -0800

    Fixed login page styling.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,43 @@
 import React from "react";
-import Nav from "./Nav.js";
 import LoginForm from "./LoginForm.js";
+import Nav from "./Nav.js";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import login_main from "../assets/login_main.svg";
+
+const useStyles = makeStyles((theme) => ({
+  mainContainer: {
+    backgroundColor: "#E3E3E3",
+    height: "90vh",
+  },
+  formContainer: {
+    width: "40%",
+  },
+}));
 
 const Login = () => {
+  const classes = useStyles();
+
   return (
     <>
       <Nav />
-      <LoginForm />
+      <Grid
+        container
+        justify="space-around"
+        alignItems="center"
+        className={classes.mainContainer}
+      >
+        <Grid item className={classes.formContainer}>
+          <Typography variant="h3" gutterBottom="true" align="center">
+            Welcome Back
+          </Typography>
+          <LoginForm />
+        </Grid>
+        <Grid item>
+          <img src={login_main} alt="authentication" />
+        </Grid>
+      </Grid>
     </>
   );
 };
