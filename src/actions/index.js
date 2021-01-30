6d2ed2ecfa2a6786b322926f3bf87b825a6a1fae
Patrick Shushereba <pshushereba@gmail.com>
2021-01-29 21:12:28 -0800

    Updated URL in index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -142,7 +142,7 @@ export const updateProfilePicture = (id, photo) => (dispatch) => {
 export const reserveRoom = () => (dispatch) => {
   dispatch({ type: RESERVE_ROOM_START });
   axiosWithAuth()
-    .get("http://localhost:5000/new")
+    .get("https://appetizen-media.herokuapp.com/new")
     .then((res) => {
       console.log("reserveRoom fired");
       dispatch({ type: RESERVE_ROOM_SUCCESS, payload: res.data });
@@ -155,7 +155,7 @@ export const reserveRoom = () => (dispatch) => {
 export const getActiveUsers = () => (dispatch) => {
   dispatch({ type: GET_ACTIVE_USERS_START });
   axiosWithAuth()
-    .get("http://localhost:5000/active")
+    .get("https://appetizen-media.herokuapp.com/active")
     .then((res) => {
       console.log("getActiveUsers fired");
       dispatch({ type: GET_ACTIVE_USERS_SUCCESS, payload: res.data });
